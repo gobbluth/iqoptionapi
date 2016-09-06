5ed437f81f4e070c8f14abf36acc056db07461aa
Gob Bluth <gobbluth@users.noreply.github.com>
2016-09-06 19:27:49 -0400

    Update getoptions.py

--- iqoptionapi/http/getoptions.py
+++ iqoptionapi/http/getoptions.py
@@ -11,22 +11,15 @@ class Getoptions(Resource):
 
     url = "/".join((Game.url, "getoptions"))
 
-
-class Getoptions(Resource):
-    """Class for IQ option getoptions resource."""
-    # pylint: disable=too-few-public-methods
-
-    #url = "https://eu.iqoption.com/api/game/getoptions?limit=30&user_balance_id=16009737"
-    url = 'getoptions'
-
-    def _get(self):
-        """Send get request for IQ Option API getpositions http resource.
+    def _get(self, balance_id):
+        """Send get request for IQ Option API getoptions http resource.
         :returns: The instace of :class:`requests.Response`.
         """
-        return self.send_http_request("GET")
+        params = dict('user_balance_id', balance_id)
+        return self.send_http_request("GET", params=params)
 
-    def __call__(self):
-        """Method to get IQ Option API getprofile http request.
+    def __call__(self, balance_id):
+        """Method to get IQ Option API getoptions http request.
         :returns: The instance of :class:`requests.Response`.
         """
-        return self._get()
+        return self._get(balance_id)
