00e57fffc321b21e75626492015b9ca2906452c7
Gob Bluth <gobbluth@users.noreply.github.com>
2016-09-06 19:23:30 -0400

    Update getoptions.py

--- iqoptionapi/http/getoptions.py
+++ iqoptionapi/http/getoptions.py
@@ -2,10 +2,18 @@
 """Module for IQ Option http getpositions resource."""
 
 from iqoptionapi.http.resource import Resource
+from iqoptionapi.http.register import Game
 
 
 class Getoptions(Resource):
-    """Class for IQ option getprofile resource."""
+    """Class for IQ option getregdata resource."""
+    # pylint: disable=too-few-public-methods
+
+    url = "/".join((Game.url, "getoptions"))
+
+
+class Getoptions(Resource):
+    """Class for IQ option getoptions resource."""
     # pylint: disable=too-few-public-methods
 
     #url = "https://eu.iqoption.com/api/game/getoptions?limit=30&user_balance_id=16009737"
