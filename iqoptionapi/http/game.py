342b4f0773546a95a6e2b2eef97a3ca56502a49a
Gob Bluth <gobbluth@users.noreply.github.com>
2016-09-06 19:22:04 -0400

    Create game.py

---
+++ iqoptionapi/http/game.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Module for IQ option game resource."""
+
+from iqoptionapi.http.resource import Resource
+
+
+class Game(Resource):
+    """Class for IQ option register resource."""
+    # pylint: disable=too-few-public-methods
+
+    url = "game"
